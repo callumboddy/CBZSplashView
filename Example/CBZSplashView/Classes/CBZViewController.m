d0b8bc033403f1b7c9fbe77718c714c10998015b
Callum Boddy <callum.boddy@gmail.com>
2014-07-22 15:37:17 +0100

    Example project

---
+++ Example/CBZSplashView/Classes/CBZViewController.m
@@ -0,0 +1,29 @@
+//
+//  CBZViewController.m
+//  CBZSplashView
+//
+//  Created by Callum Boddy on 07/22/2014.
+//  Copyright (c) 2014 Callum Boddy. All rights reserved.
+//
+
+#import "CBZViewController.h"
+
+@interface CBZViewController ()
+
+@end
+
+@implementation CBZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
