7f64d537ad01413b5c40c8ca4a7f31e5845c365a
Callum Boddy <callum.boddy@gmail.com>
2014-07-23 12:24:03 +0100

    updated pod

--- Classes/CBZSplashView.h
+++ Classes/CBZSplashView.h
@@ -10,20 +10,36 @@
 
 @interface CBZSplashView : UIView
 
-/**
- *  The starting size of the centred icon.
- */
-@property (nonatomic, assign) CGSize iconStartSize;
 
 /**
  *  This intializes the view, call addSubview on viewDidAppear.
  *
  *  @param icon            The icon image in the centre
  *  @param backgroundColor the background color of the entire view
- *  @param iconColor       the orgional mask color of the icon
  *
  *  @return The Splash view
  */
 - (instancetype)initWithIcon:(UIImage *)icon backgroundColor:(UIColor *)backgroundColor;
+
+
+/**
+ *  Call to start the animation.
+ */
+- (void)startAnimation;
+
+/**
+ *  The starting size of the centred icon.
+ */
+@property (nonatomic, assign) CGSize iconStartSize;
+
+/**
+ *  total length of animation.
+ */
+@property (nonatomic, assign) CGFloat animationDuration;
+
+/**
+ *  the color of the icon
+ */
+@property (nonatomic, strong) UIColor *iconColor;
 
 @end
